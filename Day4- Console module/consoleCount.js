e43ee54b23870eecf5bd9fb724968e614a63cbca
GulamMd <gmkhalid11@gmail.com>
2022-10-04 21:40:27 +0530

    Day 4 : Console Module in nodejs

---
+++ Day4- Console module/consoleCount.js
@@ -0,0 +1,11 @@
+//This code counts the score of remo , rj and 
+//default score which goes to none of them 
+console.count('default');
+console.count('remo');
+console.count('rj');
+console.count('remo');
+//countReset resets the count to 1 again
+console.countReset('remo'); //remo = 1
+console.count('remo');
+console.count('rj');
+console.count();
---
+++ Day4- Console module/consoleTime.js
@@ -0,0 +1,12 @@
+console.time("division");
+var x = 10;
+var y = 20;
+
+let result = x/y;
+if(result == 2) {
+    console.log("Result: %d".result);
+}
+else {
+    console.log("Result : " + result);
+}
+console.timeEnd("division");
