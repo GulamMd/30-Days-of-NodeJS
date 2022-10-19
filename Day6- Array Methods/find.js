994aa24c8df797ddddc5b27a8bd5762ad35091cd
GulamMd <gmkhalid11@gmail.com>
2022-10-19 23:40:47 +0530

    Day 6 : Array Methods

--- Day6- Array Methods/find.js
+++ Day6- Array Methods/find.js
@@ -1,3 +1,5 @@
+// The find() method returns the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned.
+
 arr = [2, 4, 6, 8];
 arr1 = [2, 3, 4, 6, 7, 8];
 
