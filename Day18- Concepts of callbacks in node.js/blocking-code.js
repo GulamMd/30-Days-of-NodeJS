19d71c83e737f0c1a888febe0669df44b79693ee
GulamMdKhalid <80089845+GulamMd@users.noreply.github.com>
2022-10-23 21:49:58 +0530

    Day 18 : Concepts of callbacks in node.js

---
+++ Day18- Concepts of callbacks in node.js/blocking-code.js
@@ -0,0 +1,25 @@
+//Example of blocking code in node.js
+
+var fs = require('fs');
+
+//For calculating execution time
+var date1 = new Date();
+var time_start = date1.getTime();
+console.log("Starting at: "+ time_start);
+console.log("Let's start reading file");
+
+//Name of the file to be read
+var file_Name = 'demo.txt';
+//Reading file synchronously
+var content = fs.readFileSync(file_Name);
+console.log("Content is: " + content);
+
+//For calculating execution time
+var date2 = new Date();
+var time_end = date2.getTime();
+console.log("Finishing at: " + time_end);
+var execution_time = time_end - time_start;
+console.log("Time of exection is: " + execution_time);
+
+//Consider it some other task in queue 
+console.log("Another task to be executed");
