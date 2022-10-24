b44140052044b4d99313855a5333f1ae88f2342a
GulamMdKhalid <80089845+GulamMd@users.noreply.github.com>
2022-10-24 16:16:39 +0530

    Day 19 : Query String in node.js

---
+++ Day19- Query String in node.js/qs-stringify.js
@@ -0,0 +1,15 @@
+// querystring.stringify(obj , sep**, eq**, options**) : This method is used to produce a URL from a collection of key and value pairs i.e. from JSON.
+// Note : ** indicates optional parameters
+
+var qs = require('querystring');
+
+var value_json = qs.stringify({ id: 1, name: ['abc', 'njera'], tutorial: '30days of node', creator : 'nodejsera' });
+console.log(value_json);
+
+var value_json_2 = qs.stringify({ id: 2, name: ['def', 'njera2'], tutorial: '30days of node', creator : 'nodejsera' },';');
+console.log("Changing the default 'sep' from '&' to ';'. An example is shown below:");
+console.log(value_json_2); 
+
+var value_json_3 = qs.stringify( {id: 3, name: ['hij', 'njera3'], tutorial: '30days of node', creator : 'nodejsera' },';',':');
+console.log("Changing the default 'eq' from '=' to ':'. An example is shown below:")
+console.log(value_json_3);
