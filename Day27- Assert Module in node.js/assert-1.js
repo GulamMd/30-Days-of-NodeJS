4fc472a5b9167f2220b72aadff63d7a8df499885
GulamMdKhalid <80089845+GulamMd@users.noreply.github.com>
2022-11-01 23:33:48 +0530

    Day 27 : Assert Module in node.js

---
+++ Day27- Assert Module in node.js/assert-1.js
@@ -0,0 +1,8 @@
+// we perform comparison operation which checks whether variable a is greater that variable b or not.
+
+const assert = require('assert');
+
+let a = 10;
+let b = 30;
+
+assert(a > b);
---
+++ Day27- Assert Module in node.js/assert-snippet-false.js
@@ -0,0 +1,13 @@
+// In this we are performing the strict comparison operation and checking whether the sum of 3 numbers is equal to the expected output or not.
+
+const assert = require('assert');
+
+function sum(a, b, c) {
+    return a + b + c;
+}
+
+let output = sum(3, 4, 5);
+console.log("Output: ", output);
+let expected_output = 15;
+console.log("Expected Output: ", expected_output);
+assert( output === expected_output , 'This is not what we expected');
